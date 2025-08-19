69a3354700660c61827690cfb328b82b7953f4d9
Roman Ožana <roman@ozana.cz>
2025-08-19 15:10:44 +0200

    Update getCertificate()
    - remove certificate validation
    - use TLSSocketOptions
    - BC: rename `Options` to `GetCertificateOptions`
    - BC: move `CertificateError` to separate file

--- src/get-certificate.ts
+++ src/get-certificate.ts
@@ -1,53 +1,135 @@
-import { CertificateError } from "./certificate-error.js";
-import {
-	type CertificateInfo,
-	fromPeerCertificate,
-} from "./certificate-info.js";
-import type { OpenSSLOptions } from "./with-openssl.js";
-import { getCertificateWithTls, type TlsOptions } from "./with-tls.js";
+import type { DetailedPeerCertificate, PeerCertificate, TLSSocket, } from "node:tls";
+import * as tls from "node:tls";
+import { TLSSocketOptions } from "node:tls";
+import { CertificateError, CertificateErrorCode } from "./certificate-error.js";
 
 /**
- * Retrieves information about a certificate for a given host.
+ * GetCertificateOptions for the getCertificate function.
  *
- * @example Retrieve certificate info for example.com
+ * - `port`: The port to connect to (default is 443).
+ * - `timeout`: The timeout for the connection in milliseconds (default is 5000).
+ * - `detailed`: If true, returns a DetailedPeerCertificate with additional information (default is false).
+ */
+export type GetCertificateOptions = TLSSocketOptions & {
+	port?: number;
+	timeout?: number;
+	detailed?: boolean;
+};
+
+/**
+ * Default options for the getCertificate function.
+ */
+const DefaultOptions: Partial<GetCertificateOptions> = {
+	port: 443, // Default port for HTTPS
+	timeout: 5000, // Default timeout of 10 seconds
+	rejectUnauthorized: false, // We'll do our own verification
+	detailed: true, // Return a DetailedPeerCertificate by default
+};
+
+/**
+ * Fetches the SSL/TLS certificate from a given host and port.
+ *
+ * @param host The hostname to connect to.
+ * @param options Optional parameters to configure the connection.
+ * @throws {CertificateError} If the connection times out or if there is an error retrieving the certificate.
+ *
+ * @example Return a PeerCertificateDetailed object:
  * ```typescript
- * import { getCertificate } from "sslko";
- * const info = await getCertificate("example.com");
- * console.log(info);
+ * import { getCertificate } from 'sslko';
+ * const cert = await getCertificate('example.com');
+ * console.log(cert);
  * ```
  *
- * @example Retrieve expired certificate info for expired.badssl.com
+ * @example Let Node.js verify the certificate for you:
  * ```typescript
- * import { getCertificate } from "sslko";
- * const info = await getCertificate("expired.badssl.com");
- * console.log(info);
+ * import { getCertificate } from 'sslko';
+ * const cert = await getCertificate('example.com', { rejectUnauthorized: true });
+ * console.log(cert);
  * ```
  *
- * Will return an object with `valid: false` and an error message.
+ * @example Return a PeerCertificateDetailed object with a custom port:
+ * ```typescript
+ * import { getCertificate } from 'sslko';
+ * const cert = await getCertificate('example.com', { port: 8443 });
+ * console.log(cert);
+ * ```
  *
- * @param host
- * @param options
+ * @example Return a PeerCertificate object:
+ * ```typescript
+ * import { getCertificate } from 'sslko';
+ * const cert = await getCertificate('example.com', { detailed: false });
+ * console.log(cert);
+ * ```
+ *
+ * @example Enable trace - useful for debugging:
+ *
+ * When enabled, TLS packet trace information is written to `stderr`. This can be used to debug TLS connection problems.
+ *
+ * ```typescript
+ * import { getCertificate } from 'sslko';
+ * const cert = await getCertificate('example.com', { enableTrace: true });
+ * ```
  */
 export async function getCertificate(
 	host: string,
-	options: Partial<TlsOptions | OpenSSLOptions> = {},
-): Promise<CertificateInfo> {
-	try {
-		// TODO add support for OpenSSL as an alternative method
-		const certificate = await getCertificateWithTls(host, options);
-		return fromPeerCertificate(certificate);
-	} catch (error) {
-		if (error instanceof CertificateError) {
-			return {
-				valid: false,
-				error: error.message,
-				code: error.code,
-			};
-		} else {
-			return {
-				valid: false,
-				error: "An unexpected error occurred while retrieving the certificate.",
-			};
-		}
+	options: Partial<GetCertificateOptions> = {},
+): Promise<DetailedPeerCertificate | PeerCertificate> {
+	const {timeout, detailed, port, ...rest} = {
+		...{host, servername: host},
+		...DefaultOptions,
+		...options,
+	};
+
+	if (port && (port < 1 || port > 65535)) {
+		throw new CertificateError(
+			"Invalid port number. Port must be between 1 and 65535.",
+			CertificateErrorCode.INVALID_PORT,
+		);
 	}
+
+	const socket: TLSSocket = tls.connect({...rest, port, timeout});
+
+	return await new Promise((resolve, reject) => {
+
+		if (timeout) {
+			socket.setTimeout(timeout, () => {
+				socket.destroy();
+				reject(
+					new CertificateError(
+						`Failed to connect to ${host}:${port} within ${timeout}ms`,
+						CertificateErrorCode.TIMEOUT,
+					),
+				)
+				;
+			});
+		}
+
+		socket.on("secureConnect", () => {
+			// @see https://github.com/oven-sh/bun/issues/21902 - Bun always failed when detailed = false
+			const cert = detailed
+				? socket.getPeerCertificate(true)
+				: socket.getPeerCertificate();
+			socket.end();
+
+			// empty certificate check
+			if (!cert || Object.keys(cert).length === 0) {
+				return reject(
+					new CertificateError(
+						"No certificate information available",
+						CertificateErrorCode.MISSING_CERTIFICATE,
+					),
+				);
+			}
+
+			resolve(cert);
+		});
+
+		socket.on("error", (error: NodeJS.ErrnoException) => {
+			const message = error?.message || "Unknown error";
+			const code =
+				(error?.code as CertificateErrorCode) ||
+				CertificateErrorCode.CERT_ERROR;
+			reject(new CertificateError(message, code));
+		});
+	});
 }
