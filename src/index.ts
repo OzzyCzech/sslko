efa13bdb4c2b2b8ea71d96653bf6dc447acb7712
Roman Ožana <roman@ozana.cz>
2026-04-24 12:07:35 +0200

    refactor: replace export * barrel with explicit named exports
    Only export the intended public API surface. Internal constants
    (DEFAULT_PORT, MIN_PORT, MAX_PORT, DEFAULT_TIMEOUT, DAY_IN_MILLISECOND,
    MIN_RSA_KEY_SIZE) are no longer part of the public contract.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-export * from "./certificate-error.js";
-export * from "./constants.js";
-export * from "./convert-peer-certificate.js";
-export * from "./get-certificate.js";
-export * from "./get-certificate-info.js";
-export * from "./types.js";
+export { CertificateError, CertificateErrorCode } from "./certificate-error.js";
+export { convertPeerCertificate } from "./convert-peer-certificate.js";
+export { getCertificate } from "./get-certificate.js";
+export {
+	checkCertificateSecurity,
+	getCertificateInfo,
+	isSelfSignedCertificate,
+	validateCertificateStructure,
+	verifyHostname,
+} from "./get-certificate-info.js";
+export type {
+	CertificateDates,
+	CertificateInfo,
+	GetCertificateOptions,
+	PeerCertificateConverted,
+} from "./types.js";
